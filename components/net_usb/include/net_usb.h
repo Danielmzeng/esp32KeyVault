e544572ccf52806ecb6b107f4f2412d7b148ac97
Daniel Zeng <danielmzeng@gmail.com>
2026-06-13 21:51:54 -0700

    Convert net_usb to vault::UsbNet class (C++)

--- components/net_usb/include/net_usb.h
+++ components/net_usb/include/net_usb.h
@@ -1,5 +1,12 @@
 #pragma once
-#include "esp_err.h"
-/* Brings up a TinyUSB NCM network interface with a static IP (10.10.0.1)
- * and a DHCP server handing out 10.10.0.2+ to the host. */
-esp_err_t net_usb_start(void);
+
+namespace vault {
+
+// Brings up a TinyUSB NCM interface with static IP 10.10.0.1 and a DHCP server
+// handing out 10.10.0.2+ to the host.
+class UsbNet {
+public:
+    void start();   // throws vault::Error on failure
+};
+
+}  // namespace vault
